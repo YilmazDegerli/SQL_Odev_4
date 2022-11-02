99ebd56d31efe04874e333999da8d276a7e2fbff
YilmazDegerli <115919886+YilmazDegerli@users.noreply.github.com>
2022-11-02 20:11:51 +0100

    Update SQL_Ödev4.sql

--- SQL_Ödev4.sql
+++ SQL_Ödev4.sql
@@ -2,6 +2,8 @@
 
 2. SELECT COUNT (DISTINCT replacement_cost) FROM film;
 
-3.
+3. SELECT COUNT(title) FROM film WHERE (title LIKE ('T%') AND rating='G');
 
-4.
+4. SELECT COUNT (country) FROM country WHERE country LIKE '_____';
+
+5. SELECT COUNT (city) FROM city WHERE city ILIKE '%r';
